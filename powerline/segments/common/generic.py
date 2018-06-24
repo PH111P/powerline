1d9bcdb378f618c2e11ffb13378d7c5c69520161
Philip Wellnitz <philipwellnitz@gmx.de>
2018-06-24 10:37:31 +0200

    add pause clicks

--- powerline/segments/common/generic.py
+++ powerline/segments/common/generic.py
@@ -30,7 +30,8 @@ def generic_shell(pl, command, highlight_groups=["generic_shell"]):
 
 
 @requires_segment_info
-def generic_growable(pl, segment_info, channel_name, segments_short, segments_long):
+def generic_growable(pl, segment_info, channel_name, segments_short, segments_long,
+        show_without_clicks=False):
     '''Returns the segments in ``segments_short`` if the channel ``channel_name`` is
     empty, otherwise the segments in ``segments_long``.
 
@@ -44,22 +45,57 @@ def generic_growable(pl, segment_info, channel_name, segments_short, segments_lo
     :param (string,string_list)_list segments_long:
         A list of (contents, highlight_groups) touples, the segments
         to be displayed in the long mode.
+    :param bool show_without_clicks:
+        Also show the segment if click support in the bar is paused.
 
     Interaction: ``#bar;ch_<fill/clear/toggle>`` fills/ clears/ toggles the
     specified channel.
     '''
 
-    if 'payloads' in segment_info and channel_name in segment_info['payloads'] and segment_info['payloads'][channel_name]:
-        return [{
-            'contents': s[0],
-            'highlight_groups': s[1],
-            'payload_name': channel_name,
-            'draw_inner_divider': True
-        } for s in segments_long]
+    if not show_without_clicks and 'payloads' in segment_info:
+        if '#pause_clicks' in segment_info['payloads']:
+            if segment_info['payloads']['#pause_clicks']:
+                return None
+
+
+    if 'payloads' in segment_info and channel_name in segment_info['payloads']:
+        if segment_info['payloads'][channel_name]:
+            return [{
+                'contents': s[0],
+                'highlight_groups': s[1],
+                'payload_name': channel_name,
+                'draw_inner_divider': True
+            } for s in segments_long]
     else:
         return [{
             'contents': s[0],
             'highlight_groups': s[1],
             'payload_name': channel_name,
             'draw_inner_divider': True
-        } for s in segments_short]
+            } for s in segments_short]
+
+@requires_segment_info
+def click_status(pl, segment_info, format_on=None, format_off='OFF'):
+    ''' Returns whether click interaction is currently paused.
+
+    :param string format_on:
+        Content to display if the bar currently allows clicks.
+    :param string format_off:
+        Content to display if the bar currently does not allow clicks.
+
+
+    Highlicght groups used: ``clicks_on`` or ``clicks_off``.
+    '''
+
+    if 'payloads' in segment_info and '#pause_clicks' in segment_info['payloads']:
+            if segment_info['payloads']['#pause_clicks']:
+                return [{
+                    'contents': format_off,
+                    'highlight_groups': ['clicks_off']
+                    }] if format_off else None
+
+
+    return [{
+        'contents': format_on,
+        'highlight_groups': ['clicks_on']
+        }] if format_on else None
