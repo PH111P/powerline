2a46fce3a200048d887e0fc56cca1049aeba9185
Philip Wellnitz <philipwellnitz@gmx.de>
2018-07-16 09:58:11 +0200

    update docs

--- powerline/segments/i3wm.py
+++ powerline/segments/i3wm.py
@@ -438,6 +438,22 @@ def active_window(pl, segment_info, cutoff=100, global_menu=False, item_length=2
         Returns the title of the currently active window.
         To enhance the global menu support, add the following to your ``.bashrc``:
 
+        .. code-block:: shell
+
+            if [ -n "$GTK_MODULES" ]; then
+                GTK_MODULES="${GTK_MODULES}:appmenu-gtk-module"
+            else
+                GTK_MODULES="appmenu-gtk-module"
+            fi
+
+            if [ -z "$UBUNTU_MENUPROXY" ]; then
+                UBUNTU_MENUPROXY=1
+            fi
+
+            export GTK_MODULES
+            export UBUNTU_MENUPROXY
+
+
         :param int cutoff:
             Maximum title length. If the title is longer, the window_class is used instead.
         :param boolean global_menu:
