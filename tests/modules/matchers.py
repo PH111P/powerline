59092193b38cdcc40f97f9dac7479aca85f1dad0
Philip Wellnitz <philipwellnitz@gmx.de>
2018-08-14 22:11:44 +0200

    cleanup

--- tests/modules/matchers.py
+++
@@ -1,6 +0,0 @@
-
-from __future__ import (unicode_literals, division, absolute_import, print_function)
-
-
-def always_true(matcher_info):
-    return True
--- tests/vim_sys_path/vim.py
+++
@@ -1,7 +0,0 @@
-
-from __future__ import (unicode_literals, division, absolute_import, print_function)
-
-import tests.modules.vim as vim
-
-
-globals().update(vim._init())
