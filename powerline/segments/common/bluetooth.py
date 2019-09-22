06e53707805bf19058520527118a561d60728523
Philip Wellnitz <philipwellnitz@gmx.de>
2019-09-23 03:22:43 +0900

    squids; add battery level

--- powerline/segments/common/bluetooth.py
+++ powerline/segments/common/bluetooth.py
@@ -21,13 +21,24 @@ class BluetoothSegment(ThreadedSegment):
             if "org.bluez.Device1" in ifac:
                 props = obj[path]["org.bluez.Device1"]
 
-                res += [{'name': str(props['Name']), 'mac': str(props['Address']),
-                    'connected': bool(props['Connected']), 'paired': bool(props['Paired'])}]
+                btpt = bus.get_object('org.bluez', path)
+                btip = dbus.Interface(btpt, 'org.freedesktop.DBus.Properties')
+
+                try:
+                    bat_per = btip.Get('org.bluez.Battery1', 'Percentage')
+                except dbus.DBusException:
+                    bat_per = None
+                if bat_per != None:
+                    bat_per = int(bat_per)
+
+                res += [{'name': str(props['Alias']), 'mac': str(props['Address']),
+                        'connected': bool(props['Connected']), 'paired': bool(props['Paired']),
+                        'battery': bat_per}]
         return res
 
     def render(self, bt_data, segment_info, format='BT {name}',
-            short_format='BT{count_connected:2}',
-            format_down=None, ignore_unconnected=True, auto_shrink=False, **kwargs):
+            short_format='BT{count_connected:2}', format_down=None, format_battery=None,
+            ignore_unconnected=True, auto_shrink=False, **kwargs):
         channel_name = 'bluetooth'
 
         if auto_shrink and not ('payloads' in segment_info and channel_name in
@@ -36,6 +47,21 @@ class BluetoothSegment(ThreadedSegment):
 
         if not bt_data:
             return None
+
+        n_bt = []
+        for b in bt_data:
+            b = b.copy()
+            if b['battery'] != None:
+                b['bat_or_empty'] = b['battery']
+            else:
+                b['bat_or_empty'] = ''
+            if format_battery:
+                b.update({'battery': format_battery.format(**b)})
+                if b['bat_or_empty'] != '':
+                    b['bat_or_empty'] = b['battery']
+                else:
+                    b['bat_or_empty'] = ''
+            n_bt += [b]
 
         count_paired = len([b for b in bt_data if b['paired']])
         count_connected = len([b for b in bt_data if b['connected']])
@@ -46,7 +72,7 @@ class BluetoothSegment(ThreadedSegment):
                 'highlight_groups': ['bluetooth'],
                 'click_values': b,
                 'payload_name': channel_name
-                } for b in bt_data if not ignore_unconnected or b['connected']]
+                } for b in n_bt if not ignore_unconnected or b['connected']]
         elif ('{count_connected' in format and count_connected) or \
                 ('{count_paired' in format and count_paired):
             return [{'contents': format.format(count_connected=count_connected,
@@ -70,6 +96,8 @@ bluetooth = with_docstring(BluetoothSegment(),
         Short format
     :param string format_down:
         Format when no device is connected
+    :param string format_battery:
+        Format used to display the battery status of the connected device.
     :param boolean ignore_unconnected:
         When listing devices, ignore
     :param boolean auto_shrink:
