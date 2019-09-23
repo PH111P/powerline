8ac34c2d90fa58f9a88fcb81009eddc798a06347
Philip Wellnitz <philipwellnitz@gmx.de>
2019-09-24 04:31:03 +0900

    net.Networkmanager: correctly extract current connection

--- powerline/segments/common/net.py
+++ powerline/segments/common/net.py
@@ -48,11 +48,21 @@ class NetworkManagerSegment(ThreadedSegment):
             cdata['connection'] = cdata['general_connection'] \
                     if cdata['general_connection'] != '' else None
             try:
-                cdata['quality'] = min(100, int(cdata['ap1_signal'])*100//80) if 'ap1_signal' in cdata else 0
-                cdata['essid'] = cdata['ap1_ssid'] if 'ap1_ssid' in cdata else None
-                cdata['security'] = cdata['ap1_security'] if 'ap1_security' in cdata else None
-                cdata['channel'] = int(cdata['ap1_chan']) if 'ap1_chan' in cdata else None
-            except Error:
+                cdata['quality'] = 0
+                cdata['essid'] = None
+                cdata['security'] = None
+                cdata['channel'] = None
+
+                apcnt = 1
+                while 'ap{0}_in-use'.format(apcnt) in cdata:
+                    if cdata['ap{0}_in-use'.format(apcnt)] == '*':
+                        cdata['quality'] = min(100, int(cdata['ap{0}_signal'.format(apcnt)])*100//80) if 'ap{0}_signal'.format(apcnt) in cdata else 0
+                        cdata['essid'] = cdata['ap{0}_ssid'.format(apcnt)] if 'ap{0}_ssid'.format(apcnt) in cdata else None
+                        cdata['security'] = cdata['ap{0}_security'.format(apcnt)] if 'ap{0}_security'.format(apcnt) in cdata else None
+                        cdata['channel'] = int(cdata['ap{0}_chan'.format(apcnt)]) if 'ap{0}_chan'.format(apcnt) in cdata else None
+                        break
+                    apcnt = apcnt + 1
+            except Exception:
                 pass
             nm_data[cdev] = cdata
 
